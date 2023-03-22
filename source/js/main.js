0aa4f0d878931265fa8c470f207e7b20b67ff670
Fitjet <andreykikin025@gmail.com>
2023-03-22 14:13:36 +0300

    добавил шапку

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,6 @@
 import {iosVhFix} from './utils/ios-vh-fix';
 import {initModals} from './modules/modals/init-modals';
+import {initAccordions} from './modules/accordion/init-accordion';
 import {Form} from './modules/form-validate/form';
 
 // ---------------------------------
@@ -18,6 +19,7 @@ window.addEventListener('DOMContentLoaded', () => {
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
     initModals();
+    initAccordions();
     const form = new Form();
     window.form = form;
     form.init();
